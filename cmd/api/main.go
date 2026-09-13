9e74b70d03c3d35459705f70dbad3040a3f64800
Blue-Onion <rawatadityasingh963@gmail.com>
2026-09-14 00:43:50 +0530

    mux

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("alll ookk"))
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Set("Content-type", "application/json")
+		w.Write([]byte(`{"status":"OK"}`))
 	})
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
+	srv := http.Server{
+		Addr:         ":3000",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+		IdleTimeout:  time.Second * 10,
+	}
+	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("server failed %v", err)
 	}
 }
